7cbc6e5325f96e27d364162406425611d585fde5
burnFrost <tectonicore@live.com>
2018-10-04 18:55:30 -0400

    Update xyz.h

--- include/xyz.h
+++ include/xyz.h
@@ -1,2 +1,21 @@
-//Physics Engine.
-//WIP
+//Physics. Alpha version.
+
+
+//3D
+float distance(vector<int>& A, vector<int>& B){
+return sqrt(pow(A[0]-B[0], 2)+pow(A[1]-B[1], 2)+pow(A[2]-B[2], 2));
+}
+
+//float angle()
+
+//float lookAt()
+
+//2D
+
+float distance2d(vector<int>& A, vector<int>& B){
+return sqrt(pow(A[0]-B[0], 2)+pow(A[1]-B[1], 2));
+}
+
+//float angle2d()
+
+//float lookAt2d()
