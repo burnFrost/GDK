37f622f5968ef3a088e659990c678a76401ea8f7
burnFrost <tectonicore@live.com>
2020-04-10 03:27:46 -0400

    Create values.h

---
+++ include/values.h
@@ -0,0 +1,22 @@
+//value types
+#define si short int
+#define simax SHRT_MAX
+
+#define usi unsigned short int
+#define usimax USHRT_MAX
+
+#define intmax INT_MAX
+
+#define uint unsigned int
+#define uintmax UINT_MAX
+
+#define longmax LONG_MAX
+
+#define ul unsigned long
+#define ulmax ULONG_MAX
+
+#define ll long long
+#define llmax LLONG_MAX
+
+#define ull unsigned long long
+#define ullmax ULLONG_MAX
